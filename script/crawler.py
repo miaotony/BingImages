b210b790e27f5558cf77a13dd2764c2fcb3c0d9f
miaotony <41962043+miaotony@users.noreply.github.com>
2025-09-19 03:28:46 +0800

    [refactor] Update User-Agent

--- script/crawler.py
+++ script/crawler.py
@@ -9,7 +9,7 @@ Bing Homepage Images
 
 @Author: MiaoTony
 @CreateTime: 20201126
-@UpdateTime: 20210218
+@UpdateTime: 20250919
 """
 
 import os
@@ -39,7 +39,7 @@ class Crawler(object):
         # self.json_en_in_url = self.host + \
         #     r"/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=EN-IN&pid=hp&ensearch=1"
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
             "Accept-Encoding": "gzip, deflate",
